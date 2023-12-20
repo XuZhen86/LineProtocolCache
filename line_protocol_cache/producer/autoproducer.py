312c8888a4259a7b5cfab4d6b4b46ed3ee9679c5
XuZhen86 <>
2023-12-20 12:09:03 +0000

    Move AutoProducer global variables to class variables

--- line_protocol_cache/producer/autoproducer.py
+++ line_protocol_cache/producer/autoproducer.py
@@ -1,6 +1,5 @@
 import os
 import sqlite3
-import time
 from queue import Queue
 from threading import Event, Thread
 from typing import Self
@@ -11,11 +10,10 @@ from influxdb_client import Point
 from line_protocol_cache.producer.flag import SQLITE_PATH, SQLITE_TIMEOUT
 from line_protocol_cache.sql import CREATE_TABLE, ENABLE_WAL, INSERT_ROW
 
-_LINE_PROTOCOL_QUEUE: Queue[str] = Queue()
-_IS_QUEUE_OPEN = Event()
-
 
 class AutoProducer:
+  QUEUE: Queue[str] = Queue()
+  IS_QUEUE_OPEN = Event()
 
   def __init__(self, cache_path: str | None = None, timeout: float | None = None) -> None:
     if cache_path is None:
@@ -30,6 +28,7 @@ class AutoProducer:
 
   def __enter__(self) -> Self:
     self._thread.start()
+    self.IS_QUEUE_OPEN.wait()
     logging.info(f'AutoProducer thread@{self._thread.native_id} has started.')
     return self
 
@@ -47,7 +46,6 @@ class AutoProducer:
 
   def _drain_queue(self) -> None:
     os.makedirs(os.path.dirname(self.cache_path), exist_ok=True)
-
     connection = sqlite3.connect(database=self.cache_path, timeout=self.timeout)
     logging.debug(f'Sqlite3 connection opened. {self.cache_path=}, {self.timeout=}')
 
@@ -55,7 +53,7 @@ class AutoProducer:
       with connection:
         connection.execute(CREATE_TABLE)
         connection.execute(ENABLE_WAL)
-      _IS_QUEUE_OPEN.set()
+      self.IS_QUEUE_OPEN.set()
       logging.debug('Sqlite3 connection init finished.')
 
       while not self._stop_thread.is_set():
@@ -64,50 +62,52 @@ class AutoProducer:
         # Intentionally draining the queue after sleeping, so that all rows up until the event are committed.
         self._insert_rows(connection, self._get_rows())
     finally:
-      _IS_QUEUE_OPEN.clear()
+      self.IS_QUEUE_OPEN.clear()
       connection.close()
       logging.debug('Sqlite3 connection closed.')
 
   def _get_rows(self) -> list[str]:
-    line_protocols: list[str] = []
-    while _LINE_PROTOCOL_QUEUE.qsize() != 0:
-      line_protocol = _LINE_PROTOCOL_QUEUE.get()
-      logging.debug(f'{line_protocol=}')
-      line_protocols.append(line_protocol)
-    return line_protocols
+    rows: list[str] = []
 
-  def _insert_rows(self, connection: sqlite3.Connection, line_protocols: list[str]) -> None:
-    if len(line_protocols) == 0:
+    while self.QUEUE.qsize() != 0:
+      row = self.QUEUE.get()
+      logging.debug(f'{row}')
+      rows.append(row)
+
+    return rows
+
+  def _insert_rows(self, connection: sqlite3.Connection, rows: list[str]) -> None:
+    if len(rows) == 0:
       return
 
     with connection:
-      connection.executemany(INSERT_ROW, [[lp] for lp in line_protocols])
-    for _ in range(len(line_protocols)):
-      _LINE_PROTOCOL_QUEUE.task_done()
-    logging.debug(f'Committed {len(line_protocols)} line protocols.')
+      connection.executemany(INSERT_ROW, [[row] for row in rows])
+    for _ in range(len(rows)):
+      self.QUEUE.task_done()
+    logging.debug(f'Committed {len(rows)} line protocols.')
 
 
 def put_line_protocol(line_protocol: str) -> None:
-  if not _IS_QUEUE_OPEN.isSet():
+  if not AutoProducer.IS_QUEUE_OPEN.isSet():
     raise ValueError('Line protocol queue is not open.')
-  _LINE_PROTOCOL_QUEUE.put(line_protocol)
+  AutoProducer.QUEUE.put(line_protocol)
 
 
 def put_line_protocols(line_protocols: list[str]) -> None:
-  if not _IS_QUEUE_OPEN.isSet():
+  if not AutoProducer.IS_QUEUE_OPEN.isSet():
     raise ValueError('Line protocol queue is not open.')
   for lp in line_protocols:
-    _LINE_PROTOCOL_QUEUE.put(lp)
+    AutoProducer.QUEUE.put(lp)
 
 
 def put_point(point: Point) -> None:
-  if not _IS_QUEUE_OPEN.isSet():
+  if not AutoProducer.IS_QUEUE_OPEN.isSet():
     raise ValueError('Line protocol queue is not open.')
-  _LINE_PROTOCOL_QUEUE.put(point.to_line_protocol())
+  AutoProducer.QUEUE.put(point.to_line_protocol())
 
 
 def put_points(points: list[Point]) -> None:
-  if not _IS_QUEUE_OPEN.isSet():
+  if not AutoProducer.IS_QUEUE_OPEN.isSet():
     raise ValueError('Line protocol queue is not open.')
   for p in points:
-    _LINE_PROTOCOL_QUEUE.put(p.to_line_protocol())
+    AutoProducer.QUEUE.put(p.to_line_protocol())
